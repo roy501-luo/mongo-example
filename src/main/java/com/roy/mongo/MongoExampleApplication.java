6a47a89493ae29c97fb2e4894e17ea0f9636cd21
roy <luojun@byhealth-ai.onaliyun.com>
2022-08-03 22:26:57 +0800

    update: fileName

--- src/main/java/com/roy/mongo/MongoExampleApplication.java
+++ src/main/java/com/roy/mongo/MongoExampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PhrMongoApplication {
+public class MongoExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PhrMongoApplication.class, args);
+		SpringApplication.run(MongoExampleApplication.class, args);
 	}
 
 }
